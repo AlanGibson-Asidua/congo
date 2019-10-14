5999012e9ad45292263033a3adf54883da0d73ef
Alan Gibson <alan.3.gibson@bt.com>
2019-10-14 13:32:00 +0100

    Load external module (not working)

--- portal/src/app/app-routing.module.ts
+++ portal/src/app/app-routing.module.ts
@@ -3,8 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CatalogueComponent } from './catalogue/catalogue.component';
 
+import { loadScript } from './external-loader';
+
+
 const routes: Routes = [
-  { path: '', component: CatalogueComponent }
+
+  { 
+    path: '', 
+    component: CatalogueComponent 
+  },
+
+  { 
+    path: 'orders', 
+    loadChildren: loadScript(
+      'http://localhost:8081/orders-orders-module.js', 
+      'orders', 
+      'OrdersModule'
+    ) 
+  }
 ];
 
 @NgModule({
